123c9bc21eb52fda0a84ed70feab81b2f8c1326b
taniguti <taniguti@wellco-corp.com>
2016-09-28 18:55:47 +0900

    LineUpBlockプラグイン追加

---
+++ plugin/LineUp_Block/Controller/Block/LineUpBlockController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the SampleBlock
+ *
+ * Copyright (C) 2016 kurozumi
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\LineUpBlock\Controller\Block;
+
+use Eccube\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class LineUpBlockController
+{
+
+    /**
+     * LineUpBlock画面
+     *
+     * @param Application $app
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(Application $app, Request $request)
+    {
+        return $app['view']->render('Block/lineup_block.twig', array(
+            // add parameter...
+        ));
+    }
+
+}
