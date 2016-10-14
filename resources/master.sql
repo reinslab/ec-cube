a6c9aee2ab1140e7d7eb3797d05f3a3217b9d49c
Hiroyuki Tsunoya <h-tsunoya@wellco-corp.com>
2016-10-14 12:41:09 +0900

    システムリソースを更新

--- resources/master.sql
+++ resources/master.sql
@@ -64,6 +64,31 @@ LOCK TABLES `mtb_customer_order_status` WRITE;
 INSERT INTO `mtb_customer_order_status` VALUES (0,'入力中',0),(1,'注文受付',1),(2,'入金待ち',99),(3,'キャンセル',3),(4,'取り寄せ中',3),(5,'発送済み',8),(6,'注文受付',2),(7,'注文未完了',99),(8,'注文未完了',99),(11,'入稿データ確認中',4),(12,'印刷中',5),(20,'印刷中',6),(21,'発送準備中',7);
 /*!40000 ALTER TABLE `mtb_customer_order_status` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `mtb_product_type`
+--
+
+DROP TABLE IF EXISTS `mtb_product_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mtb_product_type` (
+  `id` smallint(6) NOT NULL,
+  `name` longtext,
+  `rank` smallint(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mtb_product_type`
+--
+
+LOCK TABLES `mtb_product_type` WRITE;
+/*!40000 ALTER TABLE `mtb_product_type` DISABLE KEYS */;
+INSERT INTO `mtb_product_type` VALUES (1,'既製品',0),(2,'印刷商品',1);
+/*!40000 ALTER TABLE `mtb_product_type` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -74,4 +99,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-14 11:14:07
+-- Dump completed on 2016-10-14 12:08:18
