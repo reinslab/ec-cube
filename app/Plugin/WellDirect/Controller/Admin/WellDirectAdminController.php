cfa070126a534f70de99730e542ab84b77815c3c
netshop-pro <git@netshop-pro.jp>
2016-11-15 15:39:37 +0900

    #898 対応中

--- app/Plugin/WellDirect/Controller/Admin/WellDirectAdminController.php
+++ app/Plugin/WellDirect/Controller/Admin/WellDirectAdminController.php
@@ -104,13 +104,15 @@ class WellDirectAdminController extends AbstractController
     	$command = "zip " . $zip_filepath . " " . $pdf_download_dir . "/*";
     	
     	// 圧縮
-    	exec($command);
+    	exec($command, $out, $ret);
     	
     	// 一時ディレクトリ削除
     	//$this->remove_directory($pdf_download_dir);
 $app->log("pdf_download_dir = " . $pdf_download_dir);
 $app->log("zip_filename = " . $zip_filepath);
 $app->log("command = " . $command);
+$app->log($out);
+$app->log("ret = " . $ret);
    	
 /*    	【ZipArchiveはメモリ消費が激しいので使用中止】
         $zip = new \ZipArchive();
