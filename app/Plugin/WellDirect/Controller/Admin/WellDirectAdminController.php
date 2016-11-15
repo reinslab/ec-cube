35cb4dfb2b2b7a314afe67a893d3cccc2f6bf36d
netshop-pro <git@netshop-pro.jp>
2016-11-15 11:02:55 +0900

    #898 メモリ量調整

--- app/Plugin/WellDirect/Controller/Admin/WellDirectAdminController.php
+++ app/Plugin/WellDirect/Controller/Admin/WellDirectAdminController.php
@@ -65,6 +65,9 @@ class WellDirectAdminController extends AbstractController
         $zip = new \ZipArchive();
         $now = new \DateTime();
 
+		//メモリの一時的に上限を増やす
+		ini_set('memory_limit', '1024M');
+		
         $zip_filename = 'order_uploadfile_' . $now->format('YmdHis') . '.zip';
         $zip_filepath = $app['config']['image_temp_realdir'] . '/' . $zip_filename;
         $result = $zip->open($zip_filepath, \ZIPARCHIVE::CREATE | \ZIPARCHIVE::OVERWRITE);
